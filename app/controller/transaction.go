fd215a876d594de48e8922b28fb482cb939c99ca
ssherriff <stephanie@trexart.com.au>
2018-01-19 02:16:25 +1100

    Allow creation of transaction

--- app/controller/transaction.go
+++ app/controller/transaction.go
@@ -3,8 +3,13 @@ package controller
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/render"
+	"github.com/shopspring/decimal"
+	fieldbook "github.com/trexart/go-fieldbook"
 
 	"github.com/ssherriff/my-finances-app/app/model"
 	"github.com/ssherriff/my-finances-app/app/view"
@@ -20,7 +25,8 @@ func (c *Transaction) Routes() chi.Router {
 	r := chi.NewRouter()
 	// r.Use() // some middleware..
 
-	r.Get("/", c.List) // GET /transactions - read a list of transactions
+	r.Get("/", c.List)    // GET /transactions - read a list of transactions
+	r.Post("/", c.Create) // POST /transactions - create a transaction
 
 	return r
 }
@@ -33,10 +39,58 @@ func (c *Transaction) List(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	categories, err := c.TransactionDatastore.GetAllCategories()
+	if err != nil {
+		log.Println("Error getting categories: ", err)
+		return
+	}
+
 	var data = &struct {
 		Transactions []model.Transaction
+		Categories   []model.Category
 	}{
 		Transactions: transactions,
+		Categories:   categories,
 	}
 	view.RenderTemplate(w, "transactions.html", view.NewPage("Transactions", "transactions", data))
+}
+
+//Create Transaction create
+func (c *Transaction) Create(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	dateStr := r.FormValue("date")
+	description := r.FormValue("description")
+	categoryStr := r.FormValue("category")
+	amountStr := r.FormValue("amount")
+
+	// TODO Validate and error check
+
+	date, _ := time.Parse("02/01/2006", dateStr)
+	categoryID, _ := strconv.Atoi(categoryStr)
+	amount, _ := decimal.NewFromString(amountStr)
+
+	transaction := &model.Transaction{
+		Date: fieldbook.Time{
+			Time: date,
+		},
+		Description: description,
+		Category: []model.Category{
+			model.Category{ID: categoryID},
+		},
+		Amount: amount,
+	}
+
+	err := c.TransactionDatastore.SaveTransaction(transaction)
+
+	if err != nil {
+		errResponse := &ErrResponse{
+			HTTPStatusCode: http.StatusInternalServerError,
+			Err:            err,
+			ErrorText:      "Error saving transaction",
+		}
+		errResponse.Render(w, r)
+		return
+	}
+	render.Status(r, http.StatusOK)
 }
